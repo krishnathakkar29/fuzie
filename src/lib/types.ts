485e3063628d198d6977b1b5d96a9f1148e78e75
Krishna Thakkar <work.krishnat@gmail.com>
2024-11-28 21:31:13 +0530

    connections ui

---
+++ src/lib/types.ts
@@ -0,0 +1,13 @@
+import { ConnectionProviderProps } from "@/providers/connections-provider"
+
+export type ConnectionTypes = 'Google Drive' | 'Notion' | 'Slack' | 'Discord'
+
+export type Connection = {
+    title: ConnectionTypes
+    description: string
+    image: string
+    connectionKey: keyof ConnectionProviderProps
+    accessTokenKey?: string
+    alwaysTrue?: boolean
+    slackSpecial?: boolean
+  }
